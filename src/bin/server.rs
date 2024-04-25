a94cc8167cd5ba3e94b885ec7d2e5dc965529bd4
lsc_000@126.com <lsc_000@126.com>
2024-04-25 12:12:12 +0800

    1、添加两个warp请求：hello + question

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,30 @@
-use anyhow::{Ok, Result};
-use rust_web_warp_learning::socket_addr;
+use anyhow::Result;
+use rust_web_warp_learning::{
+    model::{get_question, return_error},
+    socket_addr,
+};
 use warp::Filter;
+
+async fn hello() -> Result<impl warp::Reply, warp::Rejection> {
+    let our_ids = vec![1, 3, 7, 13];
+    Ok(warp::reply::json(&our_ids))
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let hello = warp::get().map(|| "Hello, World!".to_string());
+    let hello = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::path::end())
+        .and_then(hello);
 
-    warp::serve(hello).run(socket_addr()).await;
+    let get_item = warp::get()
+        .and(warp::path("question"))
+        .and(warp::path::end())
+        .and_then(get_question);
+
+    let routes = get_item.or(hello).recover(return_error);
+
+    warp::serve(routes).run(socket_addr()).await;
 
     Ok(())
 }
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,5 +1,12 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use warp::{
+    filters::cors::CorsForbidden,
+    reject::{Reject, Rejection},
+};
+
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Question {
     id: QuestionId,
     title: String,
@@ -8,7 +15,7 @@ pub struct Question {
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct QuestionId(String);
 
 impl Question {
@@ -39,5 +46,44 @@ impl std::str::FromStr for QuestionId {
                 "question id can not be empty",
             )),
         }
+    }
+}
+
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
+pub async fn get_question() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "title".to_string(),
+        "content".to_string(),
+        None,
+    );
+
+    match question.id.0.parse::<i32>() {
+        Ok(_) => Ok(warp::reply::json(&question)),
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+    }
+}
+
+pub async fn return_error(
+    r: Rejection,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    if let Some(error) = r.find::<CorsForbidden>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            warp::http::StatusCode::FORBIDDEN,
+        ))
+    } else if r.find::<InvalidId>().is_some() {
+        Ok(warp::reply::with_status(
+            "No valid id provided".to_string(),
+            warp::http::StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found".to_string(),
+            warp::http::StatusCode::NOT_FOUND,
+        ))
     }
 }
